74cd17b8d5d4cc3a2c360b2965bfd32aeb206028
Bjarne <basicx.strgv@gmail.com>
2026-08-24 15:43:28 +0200

    Added comments, fixed naming and added class for start page import and export

--- StartPageWeb/Components/Handler/StartPageHandler.cs
+++ StartPageWeb/Components/Handler/StartPageHandler.cs
@@ -6,105 +6,114 @@ namespace StartPageWeb.Components.Handler
 {
     public static class StartPageHandler
     {
+        /// <summary>
+        /// Gets the names of all start pages.
+        /// </summary>
+        /// <remarks>
+        /// The first entry is the main start page. 
+        /// </remarks>
+        public static IReadOnlyList<string> StartPageNames
+        {
+            get
+            {
+                List<string> names = new List<string>();
+
+                foreach (StartPage startPage in _startPages)
+                {
+                    if (startPage.IsMainPage)
+                    {
+                        names.Insert(0, startPage.Name);
+                    }
+                    else
+                    {
+                        names.Add(startPage.Name);
+                    }
+                }
+
+                return names;
+            }
+        }
+
         private static List<StartPage> _startPages = [];
 
         static StartPageHandler()
         {
             _startPages.Add(new StartPage()
+            {
+                Name = "Main",
+                IsMainPage = true,
+                Columns = new List<StartPageColumn>()
                 {
-                    Name = "Main",
-                    IsMainPage = true,
-                    Columns =  new List<StartPageColumn>()
+                    new StartPageColumn()
                     {
-                        new StartPageColumn()
+                        Entries = new List<IColumnEntry>()
                         {
-                            Entries = new List<IColumnEntry>()
+                            new BookmarkGroup() { Title = "Homelab", Type = ColumnEntryType.Bookmarks },
+                            new BookmarkGroup() { Title = "Series", Type = ColumnEntryType.Bookmarks },
+                            new NoteEntry()
                             {
-                                new BookmarkGroup()
-                                {
-                                    Title = "Homelab",
-                                    Type = ColumnEntryType.Bookmarks
-                                },
-                                new BookmarkGroup()
-                                {
-                                    Title = "Series",
-                                    Type = ColumnEntryType.Bookmarks
-                                },
-                                new NoteEntry()
-                                {
-                                    Title = "Notes",
-                                    Type = ColumnEntryType.Notes,
-                                    Markdown = "**Sample** <br/> Sample Text"
-                                }
-                            }
-                        },
-                        new StartPageColumn()
-                        {
-                            Entries = new List<IColumnEntry>()
-                            {
-                                new BookmarkGroup()
-                                {
-                                    Title = "Main",
-                                    Type = ColumnEntryType.Bookmarks
-                                }, 
-                                new BookmarkGroup()
-                                {
-                                    Title = "Streaming",
-                                    Type = ColumnEntryType.Bookmarks
-                                },
-                                new BookmarkGroup()
-                                {
-                                    Title = "Payment",
-                                    Type = ColumnEntryType.Bookmarks
-                                }
-                            }
-                        },
-                        new StartPageColumn()
-                        {
-                            Entries = new List<IColumnEntry>()
-                            {
-                                new BookmarkGroup()
-                                {
-                                    Title = "Games",
-                                    Type = ColumnEntryType.Bookmarks
-                                },
-                                new BookmarkGroup()
-                                {
-                                    Title = "Bookmarks",
-                                    Type = ColumnEntryType.Bookmarks
-                                },
-                                new BookmarkGroup()
-                                {
-                                    Title = "Programming",
-                                    Type = ColumnEntryType.Bookmarks
-                                }
-                            }
-                        },
-                        new StartPageColumn()
-                        {
-                            Entries = new List<IColumnEntry>()
-                            {
-                                new BookmarkGroup()
-                                {
-                                    Title = "Shopping",
-                                    Type = ColumnEntryType.Bookmarks
-                                },
-                                new BookmarkGroup()
-                                {
-                                    Title = "3D-Printing",
-                                    Type = ColumnEntryType.Bookmarks
-                                },
-                                new BookmarkGroup()
-                                {
-                                    Title = "Other",
-                                    Type = ColumnEntryType.Bookmarks
-                                },
+                                Title = "Notes",
+                                Type = ColumnEntryType.Notes,
+                                Markdown = "**Sample** <br/> Sample Text"
                             }
                         }
+                    },
+                    new StartPageColumn()
+                    {
+                        Entries = new List<IColumnEntry>()
+                        {
+                            new BookmarkGroup() { Title = "Main", Type = ColumnEntryType.Bookmarks },
+                            new BookmarkGroup()
+                            {
+                                Title = "Streaming", Type = ColumnEntryType.Bookmarks
+                            },
+                            new BookmarkGroup() { Title = "Payment", Type = ColumnEntryType.Bookmarks }
+                        }
+                    },
+                    new StartPageColumn()
+                    {
+                        Entries = new List<IColumnEntry>()
+                        {
+                            new BookmarkGroup() { Title = "Games", Type = ColumnEntryType.Bookmarks },
+                            new BookmarkGroup()
+                            {
+                                Title = "Bookmarks", Type = ColumnEntryType.Bookmarks
+                            },
+                            new BookmarkGroup()
+                            {
+                                Title = "Programming", Type = ColumnEntryType.Bookmarks
+                            }
+                        }
+                    },
+                    new StartPageColumn()
+                    {
+                        Entries = new List<IColumnEntry>()
+                        {
+                            new BookmarkGroup()
+                            {
+                                Title = "Shopping", Type = ColumnEntryType.Bookmarks
+                            },
+                            new BookmarkGroup()
+                            {
+                                Title = "3D-Printing", Type = ColumnEntryType.Bookmarks
+                            },
+                            new BookmarkGroup() { Title = "Other", Type = ColumnEntryType.Bookmarks },
+                        }
                     }
-                });
+                }
+            });
         }
 
+        /// <summary>
+        /// Gets the start page for the given name.
+        /// </summary>
+        /// <param name="name">
+        /// The name of the start page to get.
+        /// </param>
+        /// <returns>
+        /// The <see cref="StartPageWeb.Components.Models.StartPage"/> instance
+        /// or <see langword="null"/> if no start page with the given name exists.
+        /// </returns>
         public static StartPage? GetStartPageByName(string name)
         {
             foreach (StartPage startPage in _startPages)
@@ -114,10 +123,17 @@ namespace StartPageWeb.Components.Handler
                     return startPage;
                 }
             }
-            
+
             return null;
         }
-        
+
+        /// <summary>
+        /// Gets the main start page.
+        /// </summary>
+        /// <returns>
+        /// The main <see cref="StartPageWeb.Components.Models.StartPage"/> instance
+        /// or <see langword="null"/> if no main start page exists.
+        /// </returns>
         public static StartPage? GetMainStartPage()
         {
             foreach (StartPage startPage in _startPages)
@@ -127,7 +143,7 @@ namespace StartPageWeb.Components.Handler
                     return startPage;
                 }
             }
-            
+
             return null;
         }
     }
---
+++ StartPageWeb/Components/Models/StartPageData.cs
@@ -0,0 +1,10 @@
+﻿namespace StartPageWeb.Components.Models
+{
+    public class StartPageData
+    {
+        /// <summary>
+        /// Gets or sets the lists of start pages.
+        /// </summary>
+        public List<StartPage> StartPages { get; set; }
+    }
+}
